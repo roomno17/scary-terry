7301dc10a54ac55543e4bfe74cdab092180e8ea3
roomno17 <2261021@flourbluffschools.org>
2026-02-17 12:32:29 -0600

    added an random "shake" effect to the camera as if the car hit a bump in the road

--- scary-terry/camera_3d.gd
+++ scary-terry/camera_3d.gd
@@ -1,4 +1,6 @@
 extends Camera3D
+@onready var camera: Camera3D = $"."
+
 @onready var scary_terry_left: Node3D = $"../SCARY TERRY LEFT"
 @onready var scary_terry_right: Node3D = $"../SCARY TERRY RIGHT"
 @onready var spot_light_3d: SpotLight3D = $"../SpotLight3D"
@@ -15,3 +17,26 @@ func _process(delta: float) -> void:
 			scary_terry_left.visible=false
 		elif rotation.y<-.62:
 			scary_terry_right.visible=false
+	
+
+
+
+func _on_timer_timeout() -> void:
+	var num = randi_range(1,10)
+	if num<5:
+		camera_shake()
+
+func camera_shake():
+	print("boi")
+	var x = randf_range(-.17,.17)
+	var tween = get_tree().create_tween()
+	tween.tween_property(camera,"position",Vector3(x,6.6,6.968),.1)
+	tween.tween_property(camera,"position",Vector3(x,6.4,6.968),.1)
+	tween.tween_property(camera,"position",Vector3(0,6.513,6.968),.1)
+
+
+func _on_bubble_timeout() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(camera,"position",Vector3(-.01,6.518,6.968),.05)
+	tween.tween_property(camera,"position",Vector3(.01,6.503,6.968),.05)
+	tween.tween_property(camera,"position",Vector3(0,6.513,6.968),.05)
